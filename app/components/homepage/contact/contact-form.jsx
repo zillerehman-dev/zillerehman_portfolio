e25a08c9cdd6033485c125113261241b06a1625c
Zille Rehman <zillerehman004@gmail.com>
2026-08-08 17:42:19 +0500

    fix: make contact form use same-origin api

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -35,10 +35,7 @@ function ContactForm() {
 
     try {
       setIsLoading(true);
-      const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
-        userInput
-      );
+      await axios.post("/api/contact", userInput);
 
       toast.success("Message sent successfully!");
       setUserInput({
